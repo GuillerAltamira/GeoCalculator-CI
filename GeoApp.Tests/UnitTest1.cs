2251ba6c4766a7e772b94e93c274291c5e978b7d
Guiller Altamira <altamiranoguillermo110@gmail.com>
2026-02-26 23:52:34 -0400

    Proyecto base con solución, core y tests

---
+++ GeoApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using GeoApp.Core;
+
+namespace GeoApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestAreaRectangulo()
+        {
+            double resultado = Calculadora.AreaRectangulo(10, 5);
+            Assert.Equal(50, resultado);
+        }
+    }
+}
