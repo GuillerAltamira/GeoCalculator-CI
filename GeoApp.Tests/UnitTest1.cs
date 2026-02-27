2edd9237f09e4fc2ba417943fd153521b8ee59c3
Guiller Altamira <altamiranoguillermo110@gmail.com>
2026-02-27 01:05:07 -0400

    Pruebas unitarias agregadas para Rectangulo, Circulo y Triangulo

--- GeoApp.Tests/UnitTest1.cs
+++ GeoApp.Tests/UnitTest1.cs
@@ -1,15 +1,30 @@
 ﻿using Xunit;
 using GeoApp.Core;
+using System;
 
 namespace GeoApp.Tests
 {
-    public class UnitTest1
+    public class CalculadoraTests
     {
         [Fact]
         public void TestAreaRectangulo()
         {
             double resultado = Calculadora.AreaRectangulo(10, 5);
             Assert.Equal(50, resultado);
+        }
+
+        [Fact]
+        public void TestAreaCirculo()
+        {
+            double resultado = Calculadora.AreaCirculo(1);
+            Assert.Equal(Math.PI, resultado, 5);
+        }
+
+        [Fact]
+        public void TestAreaTriangulo()
+        {
+            double resultado = Calculadora.AreaTriangulo(10, 4);
+            Assert.Equal(20, resultado);
         }
     }
 }
